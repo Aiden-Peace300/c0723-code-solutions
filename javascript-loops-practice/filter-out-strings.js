60ce99374fe17a96880fec60e6bcf50d0d106e08
Aiden Peace <47370726+Aiden-Peace300@users.noreply.github.com>
2023-07-13 13:47:44 -0700

    Update filter-out-strings.js

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -1,12 +1,14 @@
 /* exported filterOutStrings */
 function filterOutStrings(values) {
+  const result = [];
   let i = 0;
   while (i < values.length) {
     if (typeof values[i] === 'string') {
       values.splice(i, 1);
     } else {
+      result.push(values[i]);
       i++;
     }
   }
-  return values;
+  return result;
 }
