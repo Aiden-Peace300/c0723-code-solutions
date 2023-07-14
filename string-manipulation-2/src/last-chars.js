878f8456dfa055706f2e166cfad30fb0daa3e727
Aiden Peace <47370726+Aiden-Peace300@users.noreply.github.com>
2023-07-14 12:25:19 -0700

    Update last-chars.js

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -8,3 +8,11 @@ function lastChars(length, string) {
 
   return result;
 }
+
+/* 
+Alternative solution:
+function lastChars(length, string) {
+  const startIndex = Math.max(0, string.length - length);
+  return string.slice(startIndex);
+}
+*/
