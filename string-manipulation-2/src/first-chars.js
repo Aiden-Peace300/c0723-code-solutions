addb365b5d4032900e8f73a92cd0c9cf4c99fe1b
Aiden Peace <47370726+Aiden-Peace300@users.noreply.github.com>
2023-07-14 12:22:29 -0700

    Update first-chars.js

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,18 +1,4 @@
 /* exported firstChars */
 function firstChars(length, string) {
-  let result = '';
-  if (string === '') {
-    return result;
-  }
-
-  if (string.length < length) {
-    for (let i = 0; i < string.length; i++) {
-      result += string[i];
-    }
-  } else if (string.length >= length) {
-    for (let i = 0; i < length; i++) {
-      result += string[i];
-    }
-  }
-  return result;
+  return string.slice(0, length);
 }
