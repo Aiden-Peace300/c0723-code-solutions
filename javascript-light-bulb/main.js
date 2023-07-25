0640001ad7440a4b3a7562d804d8898d8f1e793d
Aiden Peace <aidenpeace300@gmail.com>
2023-07-25 19:31:25 +0000

    spacing update

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -5,6 +5,7 @@ const $circle = document.querySelector('.cirle');
 $circle.addEventListener('click', handleOffOrOn);
 
 function handleOffOrOn() {
+  // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_element_classlist_toggle
   document.querySelector('.circle').classList.toggle('dark-circle');
   document.querySelector('.body-class').classList.toggle('dark-body-class');
 }
