3d5c65d2c156db4990b6ee52957c99fccef2b195
Aiden Peace <aidenpeace300@gmail.com>
2023-07-27 19:11:22 +0000

    Updated main.js to include event.target.tagName

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -3,14 +3,16 @@ const $taskList = document.querySelector('.task-list');
 
 function handleClick(event) {
   // will output the tagName with text content
-  //   EXAMPLE:
+  //   OUTPUT EXAMPLE:
   //   event.target : <span>Learn to Code</span['>' HERE BUT WILL CAUSE WHITESPACE ERROR]
 
   console.log('event.target :', event.target);
 
   // outputting to console the tagName (element name) of which
   // the event.target fired.
-  // EXAMPLE:
+  // OUTPUT EXAMPLE:
+  // event.target.tagName : SPAN
+  console.log('event.target.tagName :', event.target.tagName);
 }
 // the addEventListener method of the '$taskList' object calls two arguments a type string and 'handleClick' function
 $taskList.addEventListener('click', handleClick);
