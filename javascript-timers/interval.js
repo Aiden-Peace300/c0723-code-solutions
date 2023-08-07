2c699a55f5b8111b19ea88c96b80d248eb5d20f3
Aiden Peace <aidenpeace300@gmail.com>
2023-08-07 21:18:20 +0000

    Adding time complexity

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -3,15 +3,18 @@ const $countdownDisplay = document.querySelector('.countdown-display');
 let i = 4;
 let j = 1000;
 
+// lines 7-18 time complexity worst case senario is O(1)
 const limitedInterval = setInterval(() => {
   $countdownDisplay.innerHTML = `${--i}`;
 
   if (i === 0) {
+    // O(1)
     $countdownDisplay.innerHTML = `~Earth Beeeelooowww Us~`;
     clearInterval(limitedInterval);
   }
 
   if (j <= 4000) {
+    // O(1)
     j += 1000;
   }
 }, j);
