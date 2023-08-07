2e6d1c5268177ca298664fa423f67d7b0d33c70a
Aiden Peace <aidenpeace300@gmail.com>
2023-08-07 22:03:12 +0000

    changing innerHTML to textContent

--- javascript-timers/timeout.js
+++ javascript-timers/timeout.js
@@ -2,5 +2,5 @@ const $message = document.querySelector('.message');
 
 // lines 4-6 time complexity worst case senario is O(1)
 setTimeout(() => {
-  $message.innerHTML = `Hello There`;
+  $message.textContent = `Hello There`;
 }, 5000);
