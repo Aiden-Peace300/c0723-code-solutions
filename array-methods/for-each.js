f8f3e7fc28a1015f54c3b07709f3e0b733cb8dbd
Aiden Peace <aidenpeace300@gmail.com>
2023-08-22 23:50:40 +0000

    Corrected the reverse values array logic to implement forEach method instead of for loop in the for-each.js file

--- array-methods/for-each.js
+++ array-methods/for-each.js
@@ -3,11 +3,8 @@ const values = [10, 1, 22, 23, 41, 5, 18, 7, 80, 9];
 console.log('In Order:');
 values.forEach((element) => console.log(element));
 
-const reverseArr = [];
-
 console.log('Reverse Order:');
-values.forEach((element) => {
-  reverseArr.unshift(element);
+values.forEach((element, index) => {
+  const reverseIndex = values.length - 1 - index;
+  console.log(values[reverseIndex]);
 });
-
-reverseArr.forEach((element) => console.log(element));
