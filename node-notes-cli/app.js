2a772afdb16929050b3bb1958a4dc83440b0d407
Aiden Peace <aidenpeace300@gmail.com>
2023-09-08 22:46:57 +0000

    Completed User can add a note

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,11 +1,12 @@
-import { readFile } from 'node:fs/promises';
+import { readFile, writeFile } from 'node:fs/promises';
 
-const argVar = process.argv.at(-1);
+const argVar = process.argv[2];
+const content = process.argv[3];
 
-if (argVar === 'read') {
+if (argVar === 'read' && content === undefined) {
   try {
-    const content = await readFile('./data.json', 'utf8');
-    const data = JSON.parse(content);
+    const response = await readFile('./data.json', 'utf8');
+    const data = JSON.parse(response);
     if (data && data.notes) {
       for (const key in data.notes) {
         if (Object.getOwnPropertyDescriptor(data.notes, key)) {
@@ -17,6 +18,29 @@ if (argVar === 'read') {
       console.error(
         'Invalid JSON format or missing "notes" object in the file.'
       );
+    }
+  } catch (err) {
+    console.error('Error reading or parsing the file:', err);
+  }
+}
+
+if (argVar === 'create' && content !== undefined) {
+  try {
+    // Read the existing data from the file
+    const response = await readFile('./data.json', 'utf8');
+    const data = JSON.parse(response);
+
+    // Check if 'data' exists and has a 'nextId' property
+    if (data && data.nextId !== undefined) {
+      // Create a new note entry
+      const newNoteId = data.nextId;
+      data.notes[newNoteId] = content;
+
+      // Increment 'nextId'
+      data.nextId++;
+
+      // Write the updated data back to the file
+      await writeFile('./data.json', JSON.stringify(data, null, 2));
     }
   } catch (err) {
     console.error('Error reading or parsing the file:', err);
