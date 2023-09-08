43d0864da2f7015070e709823c3ba69a6bb5eb0d
Aiden Peace <aidenpeace300@gmail.com>
2023-09-08 14:34:24 +0000

    Completed es6-async-await exercise

--- es6-async-await/await.js
+++ es6-async-await/await.js
@@ -8,7 +8,7 @@ const elapsed = () => `${Math.round((Date.now() - startTime) / 1000)}s -`;
 async function fetchOnce() {
   try {
     const response = await fetch('foo/bar.html');
-    const msg = await response.toString();
+    const msg = response;
     console.log(elapsed(), 'fetchOnce:', msg);
   } catch (error) {
     console.error(error);
@@ -18,15 +18,15 @@ async function fetchOnce() {
 async function fetchSeveral() {
   try {
     const response = await fetch('foo1/bar.html');
-    const msg = await response.toString();
+    const msg = response;
     console.log(elapsed(), 'fetchSeveral1:', msg);
 
-    const response2 = await fetch('foo1/bar.html');
-    const msg2 = await response2.toString();
+    const response2 = await fetch('foo2/bar.html');
+    const msg2 = response2;
     console.log(elapsed(), 'fetchSeveral2:', msg2);
 
-    const response3 = await fetch('foo1/bar.html');
-    const msg3 = await response3.toString();
+    const response3 = await fetch('foo3/bar.html');
+    const msg3 = response3;
     console.log(elapsed(), 'fetchSeveral3:', msg3);
   } catch (error) {
     console.error(error);
@@ -36,21 +36,21 @@ async function fetchSeveral() {
 async function fetchChained() {
   try {
     const response = await fetch('foo-chain/bar.html');
-    const msg = await response.toString();
+    const msg = response;
     console.log(elapsed(), 'fetchChained1:', msg);
 
     const response2 = await fetch('foo-chain/bar.html');
-    const msg2 = await response2.toString();
+    const msg2 = response2;
     console.log(elapsed(), 'fetchChained2:', msg2);
 
     const response3 = await fetch('foo-chain/bar.html');
-    const msg3 = await response3.toString();
+    const msg3 = response3;
     console.log(elapsed(), 'fetchChained3:', msg3);
   } catch (error) {
     console.error(error);
   }
 }
 
-fetchOnce()
-  .then(() => fetchSeveral())
-  .then(() => fetchChained());
+await fetchOnce();
+await fetchSeveral();
+await fetchChained();
