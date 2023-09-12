7a34f725ef21ebc6aed4d0be63d938214b5d50d2
Aiden Peace <aidenpeace300@gmail.com>
2023-09-12 19:17:56 +0000

    Completed express-post-json exercise

--- express-post-json/express-post-json-quiz-notes.md
+++ express-post-json/express-post-json-quiz-notes.md
@@ -5,6 +5,7 @@
 After completing this exercise, you should be able to discuss or answer the following questions:
 
 - What does the `express.json()` middleware do and when would you need it?
+  The express.json() middleware is used in Express.js to parse incoming JSON payloads from HTTP requests.
 
 ## Notes
 
--- express-post-json/index.js
+++ express-post-json/index.js
@@ -2,19 +2,28 @@ import express from 'express';
 
 const app = express();
 
+// mounting json middleware
+app.use(express.json());
+
 const grades = {};
 
+let nextId = 1;
+
 app.get('/api/grades', (req, res) => {
-  // Convert the grades object to an array of values
-  const response = Object.values(grades);
-  res.json(response);
+  const gradesArray = [];
+  for (const id in grades) {
+    gradesArray.push(grades[id]);
+  }
+  res.json(gradesArray);
 });
 
 app.post('/api/grades', (req, res) => {
   const newGrade = req.body;
+  newGrade.id = nextId;
+  grades[nextId] = newGrade;
   // Assign a new id and add the grade to the grades object
   res.status(201).json(newGrade);
-  app.get(newGrade);
+  nextId++;
 });
 
 app.listen(8080, () => {
