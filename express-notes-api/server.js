0a1e788470705c3198159a54b9d3ad5800f960d6
Aiden Peace <aidenpeace300@gmail.com>
2023-09-13 01:01:57 +0000

    Completed express-notes-api exercise

--- express-notes-api/server.js
+++ express-notes-api/server.js
@@ -55,6 +55,58 @@ app.post('/api/notes', async (req, res) => {
   }
 });
 
+app.delete('/api/notes/:id', async (req, res) => {
+  const noteId = req.params.id;
+
+  try {
+    const data = JSON.parse(await readFile('./data.json', 'utf-8'));
+
+    if (!(noteId > 0)) {
+      res.status(400).json({ message: 'id must be a positive integer' });
+    } else if (data.notes[noteId]) {
+      delete data.notes[noteId];
+
+      // Update the data file after deletion
+      await writeFile('./data.json', JSON.stringify(data, null, 2), 'utf-8');
+
+      res.status(204).json({ message: 'Deleted' }); // Success with no content
+    } else {
+      res.status(404).json({ message: `cannot find note with id ${noteId}` });
+    }
+  } catch (error) {
+    console.error('An unexpected error occurred:', error);
+    res.status(500).json({ error: 'An unexpected error occurred.' });
+  }
+});
+
+app.put('/api/notes/:id', async (req, res) => {
+  const noteId = req.params.id;
+  const { content } = req.body;
+
+  try {
+    if (!(noteId > 0) || !content) {
+      return res
+        .status(400)
+        .json({ error: 'Invalid note ID or content missing' });
+    }
+
+    const data = JSON.parse(await readFile('./data.json', 'utf-8'));
+
+    if (data.notes[noteId]) {
+      data.notes[noteId].content = content;
+
+      await writeFile('./data.json', JSON.stringify(data, null, 2), 'utf-8');
+
+      res.status(200).json(data.notes[noteId]); // Return the updated note
+    } else {
+      res.status(404).json({ error: 'Note not found' });
+    }
+  } catch (error) {
+    console.error('An unexpected error occurred:', error);
+    res.status(500).json({ error: 'An unexpected error occurred.' });
+  }
+});
+
 const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}!`);
