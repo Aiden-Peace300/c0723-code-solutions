0f2cec1e4a9163432d9744cb6f4e2628f3562faf
Aiden Peace <aidenpeace300@gmail.com>
2023-09-27 05:03:12 +0000

    Completed data-structures level 2

--- data-structures/queues-2.js
+++ data-structures/queues-2.js
@@ -1,3 +1,20 @@
-export function postpone(queue) {}
+import { isEmpty } from './queues-1';
 
-export function takeValueAtIndex(queue, index) {}
+export function postpone(queue) {
+  if (queue.peek() === undefined) return undefined;
+  queue.enqueue(queue.peek());
+  queue.dequeue();
+}
+
+export function takeValueAtIndex(queue, index) {
+  if (isEmpty(queue) === true) {
+    return undefined;
+  }
+
+  for (let i = 0; i < index; i++) {
+    const frontValue = queue.dequeue();
+    queue.enqueue(frontValue);
+  }
+
+  return queue.dequeue();
+}
